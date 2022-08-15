6b1510a75873b7d4615a1d0550bd0ad4d27bab2d
Lawan Idriss <iamlawanidriss@gmail.com>
2022-08-15 01:49:40 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   app.get('/filteredimage' , async (req: Request, res: Response) => {
-    const image_url = req.query.image_url.toString;();
+    const image_url = req.query.image_url.toString();
     if (!image_url) {
       res.status(400).send('image url is required');
     }
@@ -40,14 +40,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
     res.status(200).sendFile(filtered_image, () => {
       deleteLocalFiles([filtered_image]);
-    }));
+    });
+
+  });
   //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
+  });
   
 
   // Start the Server
